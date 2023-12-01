76fb743f0a384abe217ada695098b3682b7e69ea
leworgelido <rowelgelido23@gmail.com>
2023-12-01 18:58:31 +0800

    Version 9.1.1

--- db-sql/userdb.sql
+++ db-sql/userdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2023 at 11:49 AM
+-- Generation Time: Dec 01, 2023 at 11:57 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,16 +34,33 @@ CREATE TABLE `users` (
   `pass` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `image` varchar(255) NOT NULL,
-  `PhoneNo` int(11) NOT NULL,
-  `users_Address` varchar(255) NOT NULL
+  `PhoneNo` bigint(11) NOT NULL,
+  `users_Address` varchar(255) NOT NULL,
+  `have_shop` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `pass`, `created_at`, `image`, `PhoneNo`, `users_Address`) VALUES
-(131, 'rowel', 'rowel', '$2y$10$voLUjH6bmTnpju2VXoGHDOoyJcy831rMdSemWdFFneEh5PnvjPBSq', '2023-11-25 18:46:25', 'account-default-pic.jpg', 0, '');
+INSERT INTO `users` (`id`, `username`, `email`, `pass`, `created_at`, `image`, `PhoneNo`, `users_Address`, `have_shop`) VALUES
+(131, 'rowel', 'rowel', '$2y$10$voLUjH6bmTnpju2VXoGHDOoyJcy831rMdSemWdFFneEh5PnvjPBSq', '2023-11-25 18:46:25', 'account-default-pic.jpg', 0, '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_shop`
+--
+
+CREATE TABLE `users_shop` (
+  `id` int(255) NOT NULL,
+  `shop_name` varchar(30) NOT NULL,
+  `full_name` varchar(30) NOT NULL,
+  `pickup_Address` varchar(255) NOT NULL,
+  `shop_email` varchar(30) NOT NULL,
+  `shop_PhoneNo` bigint(11) NOT NULL,
+  `user_id` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -56,6 +73,13 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users_shop`
+--
+ALTER TABLE `users_shop`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,7 +87,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
+
+--
+-- AUTO_INCREMENT for table `users_shop`
+--
+ALTER TABLE `users_shop`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_shop`
+--
+ALTER TABLE `users_shop`
+  ADD CONSTRAINT `users_shop_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
